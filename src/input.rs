72e6f763907c9f845a62720fc4a5b5450c709b22
Daxanius <balder.huybreghs@gmail.com>
2026-01-15 05:19:26 +0100

    Use LazyLocks instead

--- src/input.rs
+++ src/input.rs
@@ -27,25 +27,27 @@ mod key_bindings {
 /// ASCII chars for example can just be cast to a u8 to work.
 pub mod keys {
     use super::key_bindings;
-    use once_cell::sync::Lazy;
+    use std::sync::LazyLock;
 
-    pub static KEY_RIGHT: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_right } as u8);
-    pub static KEY_LEFT: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_left } as u8);
-    pub static KEY_UP: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_up } as u8);
-    pub static KEY_DOWN: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_down } as u8);
-    pub static KEY_STRAFELEFT: Lazy<u8> =
-        Lazy::new(|| unsafe { key_bindings::key_strafeleft } as u8);
-    pub static KEY_STRAFERIGHT: Lazy<u8> =
-        Lazy::new(|| unsafe { key_bindings::key_straferight } as u8);
-    pub static KEY_FIRE: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_fire } as u8);
-    pub static KEY_USE: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_use } as u8);
+    pub static KEY_RIGHT: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_right } as u8);
+    pub static KEY_LEFT: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_left } as u8);
+    pub static KEY_UP: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_up } as u8);
+    pub static KEY_DOWN: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_down } as u8);
+    pub static KEY_STRAFELEFT: LazyLock<u8> =
+        LazyLock::new(|| unsafe { key_bindings::key_strafeleft } as u8);
+    pub static KEY_STRAFERIGHT: LazyLock<u8> =
+        LazyLock::new(|| unsafe { key_bindings::key_straferight } as u8);
+    pub static KEY_FIRE: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_fire } as u8);
+    pub static KEY_USE: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_use } as u8);
     /// When pressed, KEY_LEFT and KEY_RIGHT act like KEY_STRAFELEFT and KEY_STRAFERIGHT accordingly
-    pub static KEY_STRAFE: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_strafe } as u8);
-    pub static KEY_SPEED: Lazy<u8> = Lazy::new(|| unsafe { key_bindings::key_speed } as u8);
+    pub static KEY_STRAFE: LazyLock<u8> =
+        LazyLock::new(|| unsafe { key_bindings::key_strafe } as u8);
+    pub static KEY_SPEED: LazyLock<u8> = LazyLock::new(|| unsafe { key_bindings::key_speed } as u8);
 
     pub static KEY_ESCAPE: u8 = 27;
     pub static KEY_ENTER: u8 = b'\r';
 
+    #[must_use]
     pub fn from_char(ascii_char: char) -> Option<u8> {
         if ascii_char.is_ascii() {
             Some(ascii_char as u8)
