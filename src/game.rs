6f0b4ad40ea1baa9c1fed4a6c252b70f8c06fc8e
Daxanius <balder.huybreghs@gmail.com>
2026-01-15 19:11:24 +0100

    Use base game resolution

--- src/game.rs
+++ src/game.rs
@@ -8,8 +8,8 @@ use std::thread::sleep;
 use std::time::{Duration, Instant};
 
 /// The resolution is hardcoded in the underlying library as macro definitions.
-pub const DOOMGENERIC_RESX: usize = 640;
-pub const DOOMGENERIC_RESY: usize = 400;
+pub const DOOMGENERIC_RESX: usize = 320;
+pub const DOOMGENERIC_RESY: usize = 200;
 
 pub trait DoomGeneric {
     fn draw_frame(&mut self, screen_buffer: &[u32], xres: usize, yres: usize);
